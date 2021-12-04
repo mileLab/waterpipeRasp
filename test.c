363c6f416807465010a107d0d1639b2a1ffcb34b
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-04 12:14:34 +0100

    hotf

--- test.c
+++ test.c
@@ -172,17 +172,23 @@ static int decrypt_input_cbc(uint8_t *in, uint8_t *iv){
                       0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4, 0x11, 0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef,
                       0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17, 0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10 };
     struct AES_ctx ctx;
-    printf("before call decrypt\n");
     AES_init_ctx_iv(&ctx, key, iv);
-    printf("IV decrypt\n");
+   printf("secret to decrypt\n");
        for (size_t i = 0; i < 65; i++){
             
             printf("0x%x",encryptBuffer[i]);
         }       
     AES_CBC_decrypt_buffer(&ctx, encryptBuffer, 64);
-   
-   printf("secret to decrypt\n");
-    
+    printf("out of o!\n");
+    for (size_t i = 0; i < 65; i++){
+            
+            printf("0x%x",out[i]);
+        }   
+        printf("out of in!\n");
+        for (size_t i = 0; i < 65; i++){
+            
+            printf("0x%x",in[i]);
+        }   
     if (0 == memcmp((char*) out, (char*) in, 64)) {
         printf("SUCCESS!\n");
 	return(0);
@@ -193,7 +199,7 @@ static uint8_t encrypt_cbc(uint8_t *in, uint8_t *iv)
 {
     printf("encrypt function cbc start\n");
     
-    printf("before memecopy\n");
+   
     memcpy((uint8_t*) encryptBuffer, (uint8_t*) in, sizeof(encryptBuffer));
     struct AES_ctx ctx;
     //uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
@@ -201,7 +207,7 @@ static uint8_t encrypt_cbc(uint8_t *in, uint8_t *iv)
      
     AES_init_ctx_iv(&ctx, key, iv);
     AES_CBC_encrypt_buffer(&ctx, in, 64);
-    printf("before for\n");
+    
     for (size_t i = 0; i < 65; i++){
             
             encryptBuffer[i]=in[i];
