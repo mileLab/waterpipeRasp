8617ad8a782c511fc9f2aebe4ef0cad28ef526d1
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-04 13:35:41 +0100

    added size of encrypted buffer

--- test.c
+++ test.c
@@ -13,6 +13,7 @@
 uint8_t *encryptBuffer[64];
 
 static void phex(uint8_t* str);
+uint8_t *decrptBuffer[64];
 //static uint8_t encrypt_cbc(uint8_t *in);
 static int test_encrypt_cbc(void);
 static int test_decrypt_cbc(void);
@@ -165,10 +166,7 @@ static int test_decrypt_cbc(void)
 static int decrypt_input_cbc(uint8_t *in, uint8_t *iv){
     //uint8_t inputBuffer[64];
     //memcpy(inputBuffer, in, sizeof(inputBuffer));
-    uint8_t decrptBuffer[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
-                      0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c, 0x9e, 0xb7, 0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51,
-                      0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4, 0x11, 0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef,
-                      0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17, 0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10 };
+    
      memcpy((uint8_t*) decrptBuffer, (uint8_t*) encryptBuffer,  sizeof(encryptBuffer));
     printf("decrypt function cbc start\n");
     uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
@@ -198,14 +196,8 @@ static int decrypt_input_cbc(uint8_t *in, uint8_t *iv){
         }       
         printf("\n");
 
-    AES_CBC_decrypt_buffer(&ctx, decrptBuffer, 64);
+        AES_CBC_decrypt_buffer(&ctx, decrptBuffer, 64);
    // printf("%s",&ctx);
-    printf("out of o!\n");
-    for (size_t i = 0; i < 64; i++){
-            
-            printf("0x%x",decrptBuffer[i]);
-        }   
-        printf("\n");
         printf("out of in!\n");
         for (size_t i = 0; i < 64; i++){
             
