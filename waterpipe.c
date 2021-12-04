455b8edda925a3d95dcd1d746386b016b5061ea7
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-04 10:25:02 +0100

    hotf

--- waterpipe.c
+++ waterpipe.c
@@ -13,8 +13,8 @@
 /*=========================================================*/
 /*== INCLUDES =============================================*/
 /*=========================================================*/
-#include <wiringPi.h>
-#include <softPwm.h>
+//#include <wiringPi.h>
+//#include <softPwm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
@@ -24,11 +24,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/time.h>
-
+/*
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
-
+*/
 #include "waterpipe.h"
 #include "test.c"
 /*=========================================================*/
@@ -83,9 +83,9 @@ int main(void)
     uint8_t *inputBuffer[64];
     for(int i=0;i<65;i++)  inputBuffer[i] = 1;
     printf("before memecopy");
-    int a = sizeof(in);
-    printf("%s",a);
-   /* memcpy(inputBuffer, *in, sizeof(inputBuffer));
+    int a = sizeof((char *) in);
+   /* printf("%s",a);
+    memcpy(*inputBuffer, *in, sizeof(inputBuffer));
     struct AES_ctx ctx;
     uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
     uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
