c9ad69b71278503fa0c8ecde57602b1d46f29fd4
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-04 11:07:42 +0100

    hotf

--- test.c
+++ test.c
@@ -174,8 +174,8 @@ static int decrypt_input_cbc(uint8_t *in){
     struct AES_ctx ctx;
     printf("before call decrypt\n");
     AES_init_ctx_iv(&ctx, key, iv);
-   /* AES_CBC_decrypt_buffer(&ctx, in, 64);
-    printf("before memcmp\n");
+    AES_CBC_decrypt_buffer(&ctx, in, 64);
+   /* printf("before memcmp\n");
     if (0 == memcmp((char*) out, (char*) in, 64)) {
         printf("SUCCESS!\n");
 	return(0);
@@ -188,7 +188,7 @@ static uint8_t encrypt_cbc(uint8_t *in)
     
     for(int i=0;i<65;i++)  encryptBuffer[i] = 1;
     printf("before memecopy\n");
-    memcpy((char*) encryptBuffer, (char*) in, sizeof(encryptBuffer));
+    memcpy((uint8_t*) encryptBuffer, (uint8_t*) in, sizeof(encryptBuffer));
     struct AES_ctx ctx;
     uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
     uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
@@ -196,11 +196,11 @@ static uint8_t encrypt_cbc(uint8_t *in)
     AES_CBC_encrypt_buffer(&ctx, in, 64);
     printf("before for\n");
     for (size_t i = 0; i < 64; i++){
-            printf("inside For");
+            
             encryptBuffer[i]=in[i];
             printf("%d",in[i]);
         }       
-        printf("end of encrypt function\n");
+    printf("end of encrypt function\n");
 }
 
 static int test_encrypt_cbc(void)
