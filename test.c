839f31bb6a527d4685634251a4ad8d0df27b5e3b
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-08 20:38:15 +0100

    code clean up

--- test.c
+++ test.c
@@ -93,7 +93,7 @@ static uint8_t encrypt_cbc(uint8_t *in, uint8_t *iv)
      size_t blockSize = 64;
      // Padding check still in work
      int n = sizeof(in)/sizeof(uint8_t);
-   //  printf(n);
+    printf("%d",n);
      if (sizeof(in) < blockSize)
         {
         printf("Padding needed");
