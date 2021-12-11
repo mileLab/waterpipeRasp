9bf7b7d6b72ff42c9147f8322aad7656ce6efb71
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-11 14:52:03 +0100

    fixed blockSize

--- test.c
+++ test.c
@@ -73,7 +73,7 @@ static int decrypt_input_cbc(uint8_t *in, uint8_t *iv){
         {
         printf("remove padding");
         
-         unPaddingCipher(structWithPaddingResult->dataWithPadding,structWithPaddingResult->dataLengthWithPadding);
+        //unPaddingCipher(structWithPaddingResult->dataWithPadding,structWithPaddingResult->dataLengthWithPadding);
         } 
       for (size_t i = 0; i < 64; i++){
             decrpytBuffer[i]=encryptBuffer[i];
