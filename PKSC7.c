c042e283cdb73e527ca8008c16be0ffb0fd6ace7
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-12 11:24:48 +0100

    fix

--- PKSC7.c
+++ PKSC7.c
@@ -23,6 +23,7 @@ PKCS7_Padding* addPadding(const void* const data, const uint64_t dataLength, con
     printf("padding ammount needed %d",paddingBytesAmount); //print needed padding amount 
     paddingResult->valueOfByteForPadding = paddingBytesAmount;      
      printf("paddingResult->valueOfByteForPadding %d",paddingResult->valueOfByteForPadding);                /* according to the PKCS7 */
+    printf("dataLength %d",dataLength);
     paddingResult->dataLengthWithPadding = dataLength + paddingBytesAmount;         /* size of the final result */
     printf("Data length with padding %li",paddingResult->dataLengthWithPadding); 
     uint8_t* dataWithPadding = (uint8_t*) malloc(paddingResult->dataLengthWithPadding);
