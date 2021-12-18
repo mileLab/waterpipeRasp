253b7db50bc3712f3cdb752b10be8953440ac265
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-18 15:27:02 +0100

    hotFix

--- test.c
+++ test.c
@@ -7,7 +7,7 @@
 #define CBC 1
 #define CTR 1
 #define ECB 1
-#include "randomization.h"
+//#include "randomization.h"
 #include "aes.h"
 #include "PKSC7.h"
 
@@ -223,4 +223,41 @@ static uint8_t encrypt_cbc(uint8_t *in)
     }
     printf("end of encrypt function\n");
     printf("\n\n************************************\n");
+}
+
+
+
+// function for randomization
+int rand_comparison(const void *a, const void *b)
+{
+    (void)a;
+    (void)b;
+
+    return rand() % 2 ? +1 : -1;
+    ;
+}
+// added seed for randomizaiton (no secure random function)
+void shuffle(void *base, size_t nmemb, size_t size)
+{
+    qsort(base, nmemb, size, rand_comparison);
+}
+// final randomfunction 
+static char *random_string(char *str, size_t size)
+{
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    int i, n;
+    n = 0;
+
+    for (i = 0; n < size; n++)
+    {
+        shuffle(charset, (int)(sizeof charset), sizeof(char));
+        printf("%d\n", rand() % (int)(sizeof charset - 1));
+        int key = rand() % (int)(sizeof charset - 1);
+        str[n] = charset[key];
+    }
+
+    str[size] = '\0';
+
+    return str;
 }
