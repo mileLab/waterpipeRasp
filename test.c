c5a61485127d4df59f73efed2fdca55f65d21f98
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-18 17:30:16 +0100

    test

--- test.c
+++ test.c
@@ -265,7 +265,7 @@ static uint8_t encrypt_cbc(uint8_t *in)
     printf("\n\n************************************\n");
     printf("START ENCRYPTION\n");
     printf("\n\n************************************\n");
-    memcpy((char *)encryptBuffer, (char *)ptrToPaddingDataResult, sizeof(encryptBuffer));
+    memcpy((char *)encryptBuffer, (char *)structWithPaddingResult->dataWithPadding, sizeof(encryptBuffer));
     struct AES_ctx ctx;
     uint8_t key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
     for (uint8_t i = 0; i < 64; i++)
