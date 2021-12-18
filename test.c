6002ae95f47453afa45e7d85839a4d1989587ee6
Mile Djokic <mile.djokic@stud.fh-campuswien.ac.at>
2021-12-18 17:33:46 +0100

    test

--- test.c
+++ test.c
@@ -268,15 +268,15 @@ static uint8_t encrypt_cbc(uint8_t *in)
     memcpy((char *)encryptBuffer, (char *)structWithPaddingResult->dataWithPadding, sizeof(encryptBuffer));
     struct AES_ctx ctx;
     uint8_t key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-    for (uint8_t i = 0; i < 64; i++)
-    {
-        printf("%x", in[i]);
-        ((i + 1) % 4 == 0) ? printf("\n") : printf("\t");
-    }
     
     AES_init_ctx_iv(&ctx, key, iv);
     AES_CBC_encrypt_buffer(&ctx, encryptBuffer, 64);
-
+    for (uint8_t i = 0; i < 64; i++)
+    {
+        printf("%x", encryptBuffer[i]);
+        ((i + 1) % 4 == 0) ? printf("\n") : printf("\t");
+    }
+    
 
 
     encryptedPaket[0] = 0x79;  //Start flag
